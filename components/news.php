25a40f322c79adfd741de1e09184ca90f8dba1a0
Vadim Drozdov <drozdov.vadim.v.2003@gmail.com>
2025-07-07 12:55:19 +0300

    Добавил адаптив главной страницы

--- components/news.php
+++ components/news.php
@@ -1,8 +1,8 @@
 <section class="section news-section">
     <div class="container">
-        <h2 class="title">
+        <h1 class="title">
             Новости
-        </h2>
+        </h1>
         <div class="news-wrapper">
             <?php foreach ($news['news'] as $new): ?>
                 <?php
@@ -10,11 +10,11 @@
                 $date = substr($date, 8, 2) . "." . substr($date, 5, 2) . "." . substr($date, 0, 4);
                 ?>
 
-                    <a href="./?page=news&id=<?= htmlentities($new['id']) ?>" class="news-item">
+                <a href="./?page=news&id=<?= htmlentities($new['id']) ?>" class="news-item">
                     <div class="news-item__date"><?= $date ?></div>
-                    <h3 class="news-item__title"><?= htmlentities($new['title']) ?></h3>
+                    <h2 class="news-item__title"><?= htmlentities($new['title']) ?></h2>
                     <?= strip_tags($new['announce'], '<p><br>') ?>
-                    <div class="news-item__link" >Подробнее &rarr;</div>
+                    <div class="news-item__link">Подробнее &rarr;</div>
                 </a>
             <?php endforeach ?>
         </div>
@@ -41,9 +41,12 @@
             }
             ?>
             <?php if ($curPage == $totalPages): ?>
-                <a class="arrow-prev news-pagination__arrow" href="?page=home&p=<?= $curPage-1 ?>"><svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M1 7C0.447715 7 -4.82823e-08 7.44772 0 8C4.82823e-08 8.55228 0.447715 9 1 9L1 7ZM16.466 8.70711C16.8565 8.31658 16.8565 7.68342 16.466 7.29289L10.102 0.928931C9.7115 0.538407 9.07834 0.538407 8.68781 0.928932C8.29729 1.31946 8.29729 1.95262 8.68781 2.34315L14.3447 8L8.68781 13.6569C8.29729 14.0474 8.29729 14.6805 8.68781 15.0711C9.07834 15.4616 9.7115 15.4616 10.102 15.0711L16.466 8.70711ZM1 9L15.7589 9L15.7589 7L1 7L1 9Z" fill="#841844"/>
-</svg></a>
+                <a class="arrow-prev news-pagination__arrow" href="?page=home&p=<?= $curPage - 1 ?>"><svg width="17"
+                        height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M1 7C0.447715 7 -4.82823e-08 7.44772 0 8C4.82823e-08 8.55228 0.447715 9 1 9L1 7ZM16.466 8.70711C16.8565 8.31658 16.8565 7.68342 16.466 7.29289L10.102 0.928931C9.7115 0.538407 9.07834 0.538407 8.68781 0.928932C8.29729 1.31946 8.29729 1.95262 8.68781 2.34315L14.3447 8L8.68781 13.6569C8.29729 14.0474 8.29729 14.6805 8.68781 15.0711C9.07834 15.4616 9.7115 15.4616 10.102 15.0711L16.466 8.70711ZM1 9L15.7589 9L15.7589 7L1 7L1 9Z"
+                            fill="#841844" />
+                    </svg></a>
             <?php endif; ?>
             <?php for ($i = $start; $i <= $end; $i++): ?>
                 <?php if ($i == $curPage): ?>
@@ -54,10 +57,13 @@
             <?php endfor; ?>
 
             <?php if ($curPage < $totalPages): ?>
-                <a class="news-pagination__arrow" href="?page=home&p=<?= $curPage + 1 ?>"><svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1 7C0.447715 7 -4.82823e-08 7.44772 0 8C4.82823e-08 8.55228 0.447715 9 1 9L1 7ZM16.466 8.70711C16.8565 8.31658 16.8565 7.68342 16.466 7.29289L10.102 0.928931C9.7115 0.538407 9.07834 0.538407 8.68781 0.928932C8.29729 1.31946 8.29729 1.95262 8.68781 2.34315L14.3447 8L8.68781 13.6569C8.29729 14.0474 8.29729 14.6805 8.68781 15.0711C9.07834 15.4616 9.7115 15.4616 10.102 15.0711L16.466 8.70711ZM1 9L15.7589 9L15.7589 7L1 7L1 9Z" fill="#841844"/>
-                </svg>
-</a>
+                <a class="news-pagination__arrow" href="?page=home&p=<?= $curPage + 1 ?>"><svg width="17" height="16"
+                        viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M1 7C0.447715 7 -4.82823e-08 7.44772 0 8C4.82823e-08 8.55228 0.447715 9 1 9L1 7ZM16.466 8.70711C16.8565 8.31658 16.8565 7.68342 16.466 7.29289L10.102 0.928931C9.7115 0.538407 9.07834 0.538407 8.68781 0.928932C8.29729 1.31946 8.29729 1.95262 8.68781 2.34315L14.3447 8L8.68781 13.6569C8.29729 14.0474 8.29729 14.6805 8.68781 15.0711C9.07834 15.4616 9.7115 15.4616 10.102 15.0711L16.466 8.70711ZM1 9L15.7589 9L15.7589 7L1 7L1 9Z"
+                            fill="#841844" />
+                    </svg>
+                </a>
             <?php endif; ?>
 
 
